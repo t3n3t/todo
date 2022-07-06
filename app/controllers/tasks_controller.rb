526d73878741b5d2682e5a2613e51196e2ecd1c5
Dmitriy Manukovskiy <t3n3t@yandex.ru>
2022-07-06 19:02:02 +0300

    Add due_date to Task model

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -65,6 +65,6 @@ class TasksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def task_params
-      params.require(:task).permit(:name, :body)
+      params.require(:task).permit(:name, :body, :due_time)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_06_154737) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_06_155439) do
   create_table "tasks", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "body"
+    t.datetime "due_time"
   end
 
 end
